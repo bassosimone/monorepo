62b4716f39b117a949de6b369dce0289b9c12a90
Simone Basso <bassosimone@gmail.com>
2021-12-10 14:01:48 +0100

    feat: also clone api and backend

--- tools/config.bash
+++ tools/config.bash
@@ -1,6 +1,8 @@
 # List of repositories to sync
 repositories=(
+	git@github.com:ooni/api
 	git@github.com:ooni/asn-db-generator
+	git@github.com:ooni/backend
 	git@github.com:ooni/probe-android
 	git@github.com:ooni/probe-assets
 	git@github.com:ooni/probe-cli
