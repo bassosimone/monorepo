f9e2a30e28c47ef6ded1f13cb69ab8ce39ce5b09
Simone Basso <bassosimone@gmail.com>
2022-01-24 22:47:57 +0100

    feat: add action to build for android

--- actions/setup-defaults/lib.bash
+++ actions/setup-defaults/lib.bash
@@ -2,7 +2,7 @@ setup_defaults_depends() {
 	echo ""
 }
 
-require_path_entries() {
+setup_defaults_require_path_entries() {
 	local found_home_bin=0
 	local found_home_go_bin=0
 	for entry in $(echo $PATH | sed 's/:/ /g'); do
@@ -23,7 +23,7 @@ require_path_entries() {
 }
 
 setup_defaults_main() {
-	require_path_entries
+	setup_defaults_require_path_entries
 	require_commands curl gcc git java javac make sha256sum unzip
 	source ./actions/setup-defaults/$(platform).bash
 	setup_defaults_platform_main
