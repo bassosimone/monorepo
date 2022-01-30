88a41f3b5b3f4a5fa6d68e6a6aed3b5fe8d8aafe
Simone Basso <bassosimone@gmail.com>
2022-01-30 11:39:03 +0100

    fix(sign-android-apk): remove previously existing files

--- tools/run.d/sign-android-apk/action.bash
+++ tools/run.d/sign-android-apk/action.bash
@@ -2,7 +2,9 @@ action_main() {
 	local destdir="$(realpath ${workdir:-./output})"
 	run cd $destdir
 	run export PATH=$android_sdk/build-tools/$android_build_tools_version:$PATH
+	run rm -f app-unsigned-aligned.apk
 	run zipalign -p 4 app-unsigned.apk app-unsigned-aligned.apk
+	run rm -f app.apk
 	run apksigner sign --ks keystore.jks --out app.apk \
 		--ks-pass pass:$android_efr_keystore_password app-unsigned-aligned.apk
 }
