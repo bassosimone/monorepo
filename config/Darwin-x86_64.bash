3bab10029f7e90758f032ac1f985f9fed84e7d3f
Norbel Ambanumben <aanorbel@gmail.com>
2022-05-16 07:40:50 +0100

    added config files for

---
+++ config/Darwin-x86_64.bash
@@ -0,0 +1,15 @@
+#doc: # config/Darwin-arm64.bash
+#doc:
+#doc: This file contains darwin/arm64 specific settings.
+
+#doc:
+#doc: ## golang_tarball (string)
+#doc:
+#doc: This is the name of the golang SDK tarball for this platform.
+golang_tarball=go${golang_version}.darwin-amd64.tar.gz
+
+#doc:
+#doc: ## golang_sha256 (string)
+#doc:
+#doc: This is the SHA256 of the golang SDK tarball.
+golang_sha256=af9f9deabd6e8a9d219b695b81c19276e2cd5bbc1215741e3bb1b18e88147990
